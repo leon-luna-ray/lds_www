b31a05ec31b59e55bddc5f31dcf4af9a57b2b707
Ray Luna <leon.luna.ray@gmail.com>
2022-10-18 21:27:06 -0700

    update settings

--- lds_www/settings/base.py
+++ lds_www/settings/base.py
@@ -168,3 +168,4 @@ WAGTAILSEARCH_BACKENDS = {
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = "http://example.com"
+ALLOWED_HOSTS = ['*']
