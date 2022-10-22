b026aa6bc8b94e71e639a96e18024022dc35bf3e
Ray Luna <leon.luna.ray@gmail.com>
2022-10-22 11:05:17 -0700

    Revert "update csrf"

--- lds_www/settings/production.py
+++ lds_www/settings/production.py
@@ -2,9 +2,6 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['ldswww-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = [
-    'https://ldswww-production.up.railway.app'
-]
 
 try:
     from .local import *
