dad83d3b0c111923d8bbb1a51776f120bdde54e7
Ray Luna <leon.luna.ray@gmail.com>
2022-10-22 13:27:55 -0700

    setup aws

--- lds_www/settings/dev.py
+++ lds_www/settings/dev.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
-
+MEDIA_URL = "/media/"
 
 try:
     from .local import *
--- lds_www/settings/production.py
+++ lds_www/settings/production.py
@@ -4,6 +4,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['ldswww-production.up.railway.app']
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+
+
 COMPRESS_OFFLINE = True
 COMPRESS_CSS_FILTERS = [
     'compressor.filters.css_default.CssAbsoluteFilter',
@@ -11,6 +13,11 @@ COMPRESS_CSS_FILTERS = [
 ]
 
 COMPRESS_CSS_HASING_METHOD = 'content'
+
+
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 try:
     from .local import *
