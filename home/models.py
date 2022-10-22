95f7366758a88e5b3534e825dd19f56eaa37637a
Ray Luna <leon.luna.ray@gmail.com>
2022-10-22 11:19:57 -0700

    revert deploy ssettings

--- home/models.py
+++ home/models.py
@@ -5,20 +5,21 @@ from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 
 class HomePage(Page):
+    # pass
     # database fields
-    hero_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    body = RichTextField(null=True)
+    # hero_image = models.ForeignKey(
+    #     'wagtailimages.Image',
+    #     null=True,
+    #     blank=True,
+    #     on_delete=models.SET_NULL,
+    #     related_name='+'
+    # )
+    intro_text = RichTextField(null=True)
 
-    # config
-    max_count = 1
-    subpage_types = []
+    # # config
+    # max_count = 1
+    # subpage_types = []
     content_panels = Page.content_panels + [
-        FieldPanel('hero_image'),
-        FieldPanel('body'),
+        FieldPanel('intro_text'),
+        # FieldPanel('body'),
     ]
