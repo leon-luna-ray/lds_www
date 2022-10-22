2d23ad06563705bc1db880298d6fdc952086bf62
Ray Luna <leon.luna.ray@gmail.com>
2022-10-22 13:57:54 -0700

    add print

--- lds_www/settings/production.py
+++ lds_www/settings/production.py
@@ -24,6 +24,7 @@ MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+print('test media url' , MEDIA_URL)
 try:
     from .local import *
 except ImportError:
