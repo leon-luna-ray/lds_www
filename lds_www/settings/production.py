c1176ea5ed35fafabba0858820457a28f181b74a
Ray Luna <leon.luna.ray@gmail.com>
2022-10-22 18:03:26 -0700

    rm compression settings

--- lds_www/settings/production.py
+++ lds_www/settings/production.py
@@ -6,14 +6,6 @@ ALLOWED_HOSTS = ['ldswww-production.up.railway.app']
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 CSRF_TRUSTED_ORIGINS = ['https://ldswww-production.up.railway.app']
 
-COMPRESS_OFFLINE = True
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CssMinFilter',
-]
-
-COMPRESS_CSS_HASING_METHOD = 'content'
-
 DEFAULT_FILE_STORAGE = 'lds_www.settings.storage_backends.MediaStorage'
 
 
