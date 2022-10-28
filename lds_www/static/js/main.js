ac313c9e4dbb95bb8689a22bdf252c6af529ec9a
Ray Luna <leon.luna.ray@gmail.com>
2022-10-28 10:04:19 -0700

    update

--- lds_www/static/js/main.js
+++ lds_www/static/js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
   const body = $('html');
+  const darkMode = $('html').hasClass('dark');
 
   // Dark Mode
   $('#dark-mode-btn').on('click', function (e) {
     e.preventDefault();
+    // Todo, add an option for system theme like tailwind docs then remove code below
+    if (darkMode) {
+      localStorage.theme = 'light';
+    } else {
+      localStorage.theme = 'dark';
+    }
 
     $('#white-logo').toggleClass('hidden');
     $('#black-logo').toggleClass('hidden');
