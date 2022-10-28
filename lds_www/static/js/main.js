79d54bf7e691d84223c16da376c3392590cf9db4
Ray Luna <ray@hello-upswell.com>
2022-10-28 10:05:55 -0700

    upate

--- lds_www/static/js/main.js
+++ lds_www/static/js/main.js
@@ -1,21 +1,13 @@
 $(document).ready(function () {
   const body = $('html');
 
-
   // Dark Mode
-  function toggleDarkMode() {
-    $('#white-logo').toggleClass('hidden');
-    $('#black-logo').toggleClass('hidden');
-    body.toggleClass('dark');
-  }
-
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-    e.preventDefault();
-    toggleDarkMode();
-  });
-
   $('#dark-mode-btn').on('click', function (e) {
     e.preventDefault();
-    toggleDarkMode();
+
+    $('#white-logo').toggleClass('hidden');
+    $('#black-logo').toggleClass('hidden');
+
+    body.toggleClass('dark');
   });
 });
