349be06defa0dbbceb43f405faf68492abcd18cf
Ray Luna <ray@hello-upswell.com>
2022-10-28 10:05:11 -0700

    dark mode event listener

--- lds_www/static/js/main.js
+++ lds_www/static/js/main.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
   const body = $('html');
 
-  // Dark Mode
-  $('#dark-mode-btn').on('click', function (e) {
-    e.preventDefault();
 
+  // Dark Mode
+  function toggleDarkMode() {
     $('#white-logo').toggleClass('hidden');
     $('#black-logo').toggleClass('hidden');
-
     body.toggleClass('dark');
+  }
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    e.preventDefault();
+    toggleDarkMode();
+  });
+
+  $('#dark-mode-btn').on('click', function (e) {
+    e.preventDefault();
+    toggleDarkMode();
   });
 });
