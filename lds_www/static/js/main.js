4c04de6bba67db739c7d5dc14df9087a5dd8ad09
Ray Luna <ray@hello-upswell.com>
2022-10-28 10:29:39 -0700

    update dark mode functionality

--- lds_www/static/js/main.js
+++ lds_www/static/js/main.js
@@ -1,20 +1,35 @@
 $(document).ready(function () {
   const body = $('html');
   const darkMode = $('html').hasClass('dark');
+  // Todo find a way to refine this interaction where it will not change if it's already on the correct theme
+  const darkPreference = window.matchMedia('(prefers-color-scheme: dark)');
+
 
   // Dark Mode
-  $('#dark-mode-btn').on('click', function (e) {
-    e.preventDefault();
-    // Todo, add an option for system theme like tailwind docs then remove code below
+  function toggleDarkMode() {
     if (darkMode) {
       localStorage.theme = 'light';
+      $('#white-logo').toggleClass('hidden');
+      $('#black-logo').toggleClass('hidden');
     } else {
       localStorage.theme = 'dark';
+      $('#white-logo').toggleClass('hidden');
+      $('#black-logo').toggleClass('hidden');
     }
-
-    $('#white-logo').toggleClass('hidden');
-    $('#black-logo').toggleClass('hidden');
-
     body.toggleClass('dark');
+  }
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    e.preventDefault();
+    if ((!darkMode && darkPreference.matches) || (darkMode && !darkPreference.matches)) {
+      toggleDarkMode();
+    }
   });
+
+  $('#dark-mode-btn').on('click', function (e) {
+    e.preventDefault();
+    toggleDarkMode();
+  });
+
+  console.log(darkPreference)
 });
