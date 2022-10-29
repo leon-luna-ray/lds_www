9fa914db248fb38daa91f2159db3dad175218598
Ray Luna <leon.luna.ray@gmail.com>
2022-10-29 15:03:59 -0700

    add generic content block to home page

--- apps/home/models.py
+++ apps/home/models.py
@@ -5,6 +5,7 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.images.blocks import ImageChooserBlock
 
 from apps.base.models.pages import BasePage, BasePageWithOptions
+from apps.base.models.blocks import BodySectionBlock
 
 
 class HomePage(BasePage):
@@ -13,7 +14,7 @@ class HomePage(BasePage):
         [
             ('carousel_item', ImageChooserBlock(
                 required=True)
-            ),
+             ),
         ],
         block_counts={
             'carousel_item': {'min_num': 2, 'max_num': 5}
@@ -22,6 +23,13 @@ class HomePage(BasePage):
         null=True,
     )
 
+    content = StreamField([
+        ('sections', BodySectionBlock())
+    ],
+        blank=False,
+        null=True,
+        max_num=1,
+    )
 
     # Link Block to other pages (block (image + text + page or url))
     # Annoucments/promotions (block)
@@ -35,14 +43,16 @@ class HomePage(BasePage):
     content_panels = Page.content_panels + [
         FieldPanel('intro_text'),
         FieldPanel('banner_carousel'),
+        FieldPanel('content'),
     ]
 
 # About
+
+
 class AboutPage(BasePageWithOptions):
 
-    content_panels = BasePageWithOptions.intro_with_image_panel +[]
+    content_panels = BasePageWithOptions.intro_with_image_panel + []
 
     # config
     max_count = 1
     template = 'home/about.html'
-
