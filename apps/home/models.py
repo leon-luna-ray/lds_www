48d37152f0658d54c4532b2e5a6a5b2b295d4e42
Ray Luna <leon.luna.ray@gmail.com>
2022-11-14 22:57:50 -0800

    add content block to services landing page

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.fields import RichTextField, StreamField
+from wagtail.fields import StreamField
 from wagtail.admin.panels import FieldPanel
 from wagtail.images.blocks import ImageChooserBlock
 
